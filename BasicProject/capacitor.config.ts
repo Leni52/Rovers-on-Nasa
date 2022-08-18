84953f924da2c95a7aa921b8bab5e3c300db203b
Lenche Petrov <lenche.petrov@scalefocus.com>
2022-08-18 10:41:07 +0300

    adding capacitor

---
+++ BasicProject/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'basic-project',
+  webDir: 'dist/basic-project',
+  bundledWebRuntime: false
+};
+
+export default config;
